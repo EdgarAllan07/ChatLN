5b2b3dc61f870bcba5e61d403d8ee58174d9a103
Allan <108568433+RoshPlay@users.noreply.github.com>
2023-06-27 20:37:11 -0600

    6/072023
    Pa tu

--- client/public/code.js
+++ client/public/code.js
@@ -15,9 +15,27 @@ async function sentPayment(){
     const result = await window.webln.sendPayment(invoice);
     var ventana = document.querySelector('.popup');
     if(result.preimage != ""){
-      ventana.innerHTML = '<H2>Transacion completada</H2> <br>   <button id="Cerrar" onclick="hidePopup()">Cerrar</button>'
+      ventana.innerHTML = '<H2>Transacion completada</H2> <br>   <img src="img/verifcado.png" alt="verificacion" class=verify>  <button id="Cerrar" onclick="hidePopup()">Cerrar</button>'
     }
   }catch(error ){
     console.error(error)
   }
+}
+
+async function keySend(){
+  var pubkey = document.getElementById('pubkey').value;
+  var amount = document.getElementById('cantidad').value
+  await webln.enable();
+const result = await webln.keysend({
+  destination: pubkey, 
+  amount: amount, 
+  customRecords: {
+      "34349334": "HELLO AMBOSS"
+  }
+});
+var ventana = document.querySelector('.popup');
+console.log(result);
+if(result.preimage != ""){
+ventana.innerHTML = '<H2>Transacion completada</H2> <br>   <button id="Cerrar" onclick="hidePopup()">Cerrar</button>'
+}
 }
