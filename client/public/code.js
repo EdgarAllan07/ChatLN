6b35e2755e7f56005f9d633ba9fde30b117dadf1
Allan <108568433+RoshPlay@users.noreply.github.com>
2023-06-27 21:06:34 -0600

    Actualizacion 2.0

--- client/public/code.js
+++ client/public/code.js
@@ -5,6 +5,7 @@ function hidePopup() {
 }
 
 async function sentPayment(){
+  var cond = ""
   if (typeof window.webln === "undefined") {
     return alert("No WebLN available.");
   }
@@ -13,13 +14,13 @@ async function sentPayment(){
     var factura11 = document.getElementById('f11').value;
     const invoice = factura11;
     const result = await window.webln.sendPayment(invoice);
+    cond = result.preimage;
     var ventana = document.querySelector('.popup');
-    if(result.preimage != ""){
-      ventana.innerHTML = '<H2>Transacion completada</H2> <br>   <img src="img/verifcado.png" alt="verificacion" class=verify>  <button id="Cerrar" onclick="hidePopup()">Cerrar</button>'
-    }
+    
   }catch(error ){
     console.error(error)
   }
+  cond = "";
 }
 
 async function keySend(){
@@ -36,6 +37,6 @@ const result = await webln.keysend({
 var ventana = document.querySelector('.popup');
 console.log(result);
 if(result.preimage != ""){
-ventana.innerHTML = '<H2>Transacion completada</H2> <br>   <button id="Cerrar" onclick="hidePopup()">Cerrar</button>'
+ventana.innerHTML = '<H2>Transacion completada</H2> <br>   <img src="img/verify.png" alt="verificacion" class=verify><button id="Cerrar" onclick="hidePopup()">Cerrar</button>'
 }
 }
