6ca1d173ec596e15f43b7a607455a112d72af19c
Allan <108568433+RoshPlay@users.noreply.github.com>
2023-06-29 01:58:50 -0600

    Update code.js

--- client/public/code.js
+++ client/public/code.js
@@ -8,6 +8,7 @@ function hidePopup() {
   document.querySelector('.overlay').style.display = 'none';
   document.querySelector('.popup').style.display = 'none';
   document.querySelector('.Mostrar').style.display = 'none';
+  document.getElementById('f11').value = "";
 }
 //Enviar el pago
 async function sentPayment() {
