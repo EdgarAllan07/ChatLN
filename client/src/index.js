40f36577850a8fc595b5e8dc4bb295cc9b157001
alemanjosh7 <josuealeman2003@gmail.com>
2023-06-28 22:53:10 -0600

    Agregando funcionalidades y resolviendo bugs

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App className="d-flex justify-content-center align-items-center " />
   </React.StrictMode>,
   document.getElementById("root")
 );
